abf716fb1a7c4bf64909730f14acf2e33c94651f
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-11-15 21:44:44 +0530

    basic login form template and styling

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Real Estate Company - Login</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <div class="lg-frm">
+        <form action="./inc/login.php" method="post">
+            <input type="text" name="username">
+            <input type="password" name="password">
+            <button type="submit" class="lg-btn">Login</button>
+        </form>
+    </div>
+</body>
+</html>
