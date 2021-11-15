6861007d896b6888a72629bd8f11905eb0b83470
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-11-16 02:16:57 +0530

    dashboard page style updated

--- dashboard.php
+++ dashboard.php
@@ -4,19 +4,19 @@
 <body>
     <div class="main-sec">
         <div class="sum-box">
-            <i class="fas fa-home"></i>
+            <img src="./images/home.png" alt="">
             <h2>Number of Properties</h2>
-            <h2>25</h2>
+            <h2 class="num">25</h2>
         </div>
         <div class="sum-box">
-            <i class="fas fa-user-friends"></i>
+            <img src="./images/customers.png" alt="">
             <h2>Number of Customers</h2>
-            <h2>67</h2>
+            <h2 class="num">67</h2>
         </div>
         <div class="sum-box">
-            <i class="fas fa-people-arrows"></i>
+            <img src="./images/workers.png" alt="">
             <h2>Number of Workers</h2>
-            <h2>33</h2>
+            <h2 class="num">33</h2>
         </div>
     </div>
 </body>
